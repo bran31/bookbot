57b80a4fa2098096265354dfb32f56da885b89d8
bran31 <bran31@gmail.com>
2025-05-22 20:51:29 -0400

    testing

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+def main():
+    path_to_file = "books/frankenstein.txt"
+    get_book_text(path_to_file)
+    print(get_book_text(path_to_file))
+main()
