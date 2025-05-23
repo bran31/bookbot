c3ea37fbec4b72ca203510417db15ba39abb203f
bran31 <bran31@gmail.com>
2025-05-22 21:33:22 -0400

    adding report

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import *
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -5,6 +6,10 @@ def get_book_text(path_to_file):
     return file_contents
 def main():
     path_to_file = "books/frankenstein.txt"
-    get_book_text(path_to_file)
-    print(get_book_text(path_to_file))
+    book = get_book_text(path_to_file)
+    # print(book)
+    num = count_letters(book)
+    letters = repeats(book)
+    print(f"{num} words found in the document")
+    print(letters)
 main()
---
+++ stats.py
@@ -0,0 +1,25 @@
+def count_letters(file):
+    number = file.split()
+    return len(number)
+
+def repeats(file):
+    letters = file.lower()
+    
+    p = 0
+    t = 0
+    c = 0
+    e = 0
+    for letter in letters:
+        if letter == 'p':   
+             p += 1
+        elif letter == 't':
+            t += 1
+        elif letter == 'c':
+            c += 1
+        elif letter == 'e':
+            e += 1
+    ans = {'t' : t,'p' : p,'c' : c, 'e' : e}
+    return ans   
+
+def report(file):
+    pass
